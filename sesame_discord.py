2d008420cec364cf38db15e94281655c61971ce5
ば <masaya.kurata@std.code.ac.jp>
2022-04-18 00:13:52 +0900

    task_idの詳細を見る際に10回まで１秒ごとに回すことにしました

--- sesame_discord.py
+++ sesame_discord.py
@@ -19,32 +19,46 @@ JST = datetime.timezone(t_delta, 'JST')
 
 
 def check_sesame_status():
-    res = req.get(url=f'https://api.candyhouse.co/public/sesame/{SESAME_DEVICE_ID}',
+    url = f'https://api.candyhouse.co/public/sesame/{SESAME_DEVICE_ID}'
+    res = req.get(url=url,
                   headers={"Authorization": SESAME_TOKEN}
                   )
     return res
 
-def check_sesame_task(return_task_id):
-    res = req.get(url=f'https://api.candyhouse.co/public/action-result?task_id={return_task_id}',
+
+def check_sesame_task(task_id):
+    url = f'https://api.candyhouse.co/public/action-result?task_id={task_id}'
+    res = req.get(url=url,
                   headers={"Authorization": SESAME_TOKEN}
                   )
     return res
+
 
 def turn_sesame(task_command="lock", discord_message=None):
     sesame_status = check_sesame_status().json()
-    lock_status = sesame_status["locked"]
-    battery_status = sesame_status["battery"]
-    responsive = sesame_status["responsive"]
+    # sesame_status["locked"]
+    # sesame_status["battery"]
+    # sesame_status["battery"]
 
-    # 10秒ほどおかないとDEVICE_IS_BUSYになり判定がバグる
+    # 10秒ほどおかないとDEVICE_IS_BUSYになり判定がバグる そうでもない？
     time.sleep(1)
-    #print(sesame_status)
-    if lock_status is True and task_command != "lock" or lock_status is False and task_command != "unlock":
-        res = req.post(url=f'https://api.candyhouse.co/public/sesame/{SESAME_DEVICE_ID}',
+
+    # print(sesame_status)
+    if sesame_status["locked"] is True and task_command != "lock" or\
+       sesame_status["locked"] is False and task_command != "unlock":
+        url = f'https://api.candyhouse.co/public/sesame/{SESAME_DEVICE_ID}'
+        res = req.post(url=url,
                        headers={"Authorization": SESAME_TOKEN},
                        data=json.dumps({"command": task_command})
                        )
-        time.sleep(3)
+
+        loop_count = 0
+        while check_sesame_task(res.json()["task_id"]).json()["status"] == "processing":
+            loop_count = loop_count + 1
+            # print(f"Retry: {loop_count}")
+            if loop_count > 11:
+                return {"res_result": "loop", "status": None, "task_result": None}
+            time.sleep(1)
 
         if "task_id" in res.json():
             task_result = check_sesame_task(res.json()["task_id"])
@@ -56,9 +70,8 @@ def turn_sesame(task_command="lock", discord_message=None):
         elif "error" in res.json():
             dict_result = "failed"
             dict_task_result = None
-        return {"response_result":dict_result, "status":sesame_status, "task_result":dict_task_result}
-
-    return
+        return {"res_result": dict_result, "status": sesame_status, "task_result": dict_task_result}
+    return {"res_result": None, "status": None, "task_result": None}
 
 
 @client.event
@@ -71,21 +84,34 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.guild.id != DISCORD_SERVER_ID or message.channel.name != DISCORD_CHANNEL_NAME or message.author == client.user:
+    if message.guild.id != DISCORD_SERVER_ID or\
+       message.channel.name != DISCORD_CHANNEL_NAME or\
+       message.author == client.user:
         return
 
-    if message.content.startswith('/lock') or message.content.startswith('/unlock'):
+    if message.content.startswith('/lock') or\
+       message.content.startswith('/unlock'):
+
         task_command = message.content[1:]
         result = turn_sesame(task_command)
-        #print(result)
+        print(result)
         now_time = datetime.datetime.now(JST).time().strftime('%X')
-        if result["response_result"] == "success" and result["task_result"]["successful"] is True:
-            await message.channel.send(f'[{now_time}] [{result["status"]["battery"]}%] {task_command} success!')
-        elif result == "failed" or result["task_result"]["successful"] is False:
-            await message.channel.send(f'[{now_time}] [{result["status"]["battery"]}%] {task_command} failed.')
-            await message.channel.send(f'[{now_time}] {result["task_result"]["error"]}')
-        if result["status"]["battery"] <= 40:
-            await message.channel.send(f'バッテリー残量警告[{result["status"]["battery"]}%]: 交換をおすすめします')
+        if result["res_result"] is not None:
+            if result["res_result"] == "success" and\
+               result["task_result"]["successful"] is True:
+                await message.channel.send(f'[{now_time}] [{result["status"]["battery"]}%] {task_command} success!')
+
+            elif result["res_result"] == "failed" or\
+                 result["task_result"]["successful"] is False:
+                await message.channel.send(f'[{now_time}] [{result["status"]["battery"]}%] {task_command} failed.')
+                await message.channel.send(f'[{now_time}] {result["task_result"]["error"]}')
+
+            elif result["res_result"] == "loop":
+                await message.channel.send(f'[{now_time}] [{result["status"]["battery"]}%] {task_command} failed.')
+                await message.channel.send(f'[{now_time}] sesameサーバーが混み合っている可能性があります。再試行して同様のエラーが発生するならばsesameアプリからの操作をお勧めします')
+
+            if result["status"]["battery"] <= 40:
+                await message.channel.send(f'バッテリー残量警告[{result["status"]["battery"]}%]: バッテリーの交換をおすすめします')
 
 
 client.run(DISCORD_TOKEN)
