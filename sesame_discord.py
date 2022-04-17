e957a9b840805a4870cbaa04d7872c9cce05bb1c
ば <masaya.kurata@std.code.ac.jp>
2022-04-17 23:23:13 +0900

    なんかわからんけどwifiモジュールが死んでもsuccessになる問題をfix

--- sesame_discord.py
+++ sesame_discord.py
@@ -24,6 +24,11 @@ def check_sesame_status():
                   )
     return res
 
+def check_sesame_task(return_task_id):
+    res = req.get(url=f'https://api.candyhouse.co/public/action-result?task_id={return_task_id}',
+                  headers={"Authorization": SESAME_TOKEN}
+                  )
+    return res
 
 def turn_sesame(task_command="lock", discord_message=None):
     sesame_status = check_sesame_status().json()
@@ -32,28 +37,36 @@ def turn_sesame(task_command="lock", discord_message=None):
     responsive = sesame_status["responsive"]
 
     # 10秒ほどおかないとDEVICE_IS_BUSYになり判定がバグる
-    time.sleep(10)
+    time.sleep(1)
+    #print(sesame_status)
     if lock_status is True and task_command != "lock" or lock_status is False and task_command != "unlock":
-    #if True:
         res = req.post(url=f'https://api.candyhouse.co/public/sesame/{SESAME_DEVICE_ID}',
                        headers={"Authorization": SESAME_TOKEN},
                        data=json.dumps({"command": task_command})
                        )
+        time.sleep(3)
+
         if "task_id" in res.json():
-            return True, sesame_status
+            task_result = check_sesame_task(res.json()["task_id"])
+            if "error" not in task_result.json():
+                dict_result = "success"
+            elif "error" in task_result.json():
+                dict_result = "failed"
+            dict_task_result = task_result.json()
         elif "error" in res.json():
-            return False, sesame_status
-        else:
-            return res.json(), sesame_status
-    return "identical"
+            dict_result = "failed"
+            dict_task_result = None
+        return {"response_result":dict_result, "status":sesame_status, "task_result":dict_task_result}
+
+    return
 
 
 @client.event
 async def on_ready():
-    url = f"https://api.candyhouse.co/public/sesames"
+    url = "https://api.candyhouse.co/public/sesames"
     headers = {"Authorization": SESAME_TOKEN}
-    res = req.get(url, headers=headers)
-    return res
+    response = req.get(url, headers=headers)
+    return response
 
 
 @client.event
@@ -63,17 +76,16 @@ async def on_message(message):
 
     if message.content.startswith('/lock') or message.content.startswith('/unlock'):
         task_command = message.content[1:]
-        result, sesame_status = turn_sesame(task_command)
+        result = turn_sesame(task_command)
+        #print(result)
         now_time = datetime.datetime.now(JST).time().strftime('%X')
-        if result is True:
-            await message.channel.send(f'[{now_time}] [{sesame_status["battery"]}%] {task_command} success!')
-        elif result is False:
-            await message.channel.send(f'[{now_time}] [{sesame_status["battery"]}%] {task_command} failed.')
-            await message.channel.send(f'[{now_time}] sesame本体は他の端末で使用中か、インターネット接続に問題があるため、WiFiモジュールが青点滅しているか確認してください。')
-        else:
-            await message.channel.send(f'[{now_time}] [{sesame_status["battery"]}%] {result}')
-        if sesame_status["battery"] <= 40:
-            await message.channel.send(f'バッテリー残量警告[{battery_percent}%]: 交換をおすすめします')
+        if result["response_result"] == "success" and result["task_result"]["successful"] is True:
+            await message.channel.send(f'[{now_time}] [{result["status"]["battery"]}%] {task_command} success!')
+        elif result == "failed" or result["task_result"]["successful"] is False:
+            await message.channel.send(f'[{now_time}] [{result["status"]["battery"]}%] {task_command} failed.')
+            await message.channel.send(f'[{now_time}] {result["task_result"]["error"]}')
+        if result["status"]["battery"] <= 40:
+            await message.channel.send(f'バッテリー残量警告[{result["status"]["battery"]}%]: 交換をおすすめします')
 
 
 client.run(DISCORD_TOKEN)
